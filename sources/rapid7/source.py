4d62c46758ae7d08097acdfc1f03800a366ab839
Joey Dreijer <jdreijer@specterops.io>
2025-10-16 12:55:31 +0200

    Added fetching assets and vulnerablities from rapid7 api

--- sources/rapid7/source.py
+++ sources/rapid7/source.py
@@ -2,6 +2,7 @@ from dlt.sources.rest_api import rest_api_resources
 from dlt.sources.rest_api.typing import RESTAPIConfig
 from dlt.sources.helpers.rest_client.auth import HttpBasicAuth
 import dlt
+import requests
 
 
 @dlt.source
@@ -9,18 +10,22 @@ def rapid7_source(
     username: str = dlt.secrets.value,
     password: str = dlt.secrets.value,
     host: str = dlt.secrets.value,
+    verify: bool = True,
 ):
+    session = requests.Session()
+    session.verify = verify
     config: RESTAPIConfig = {
         "client": {
             "base_url": host,
             "auth": HttpBasicAuth(username, password),
+            "session": session,
         },
         "resource_defaults": {
             "primary_key": "id",
             "write_disposition": "merge",
             "endpoint": {
                 "params": {
-                    "per_page": 100,
+                    "size": 500,
                 },
             },
         },
@@ -28,7 +33,13 @@ def rapid7_source(
             {
                 "name": "assets",
                 "endpoint": {
-                    "path": "assets",
+                    "path": "/api/3/assets",
+                },
+            },
+            {
+                "name": "vulnerabilities",
+                "endpoint": {
+                    "path": "/api/3/assets/{resources.assets.id}/vulnerabilities"
                 },
             },
         ],
