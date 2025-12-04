8c7690ec91ad364a35afe3b829465e5a09867b19
Joey Dreijer <jdreijer@specterops.io>
2025-12-04 12:24:28 -0500

    Updated model for random data generator

--- opengraph_dlt/sources/dummy/models/random_data.py
+++ opengraph_dlt/sources/dummy/models/random_data.py
@@ -1,16 +1,11 @@
 from collections.abc import Iterator
-from pydantic import BaseModel, SkipValidation
-
 from opengraph_dlt.sources.dummy.models.graph import (
     Node,
     NodeProperties,
     BaseResource,
 )
-
-# from opengraph_dlt.sources.shared.models.entries import Edge, EdgePath, EdgeProperties
+from opengraph_dlt.sources.shared.models.entries import Edge, EdgePath
 from opengraph_dlt.sources.shared.docs import graph_resource, NodeDef, EdgeDef
-from typing_extensions import TypedDict
-from dataclasses import dataclass
 
 
 class ExtendedProperties(NodeProperties):
@@ -19,30 +14,6 @@ class ExtendedProperties(NodeProperties):
 
 class RandomDataNode(Node):
     properties: ExtendedProperties
-
-
-class EdgePath(BaseModel):
-    value: str
-    match_by: str
-
-
-class Edge(BaseModel):
-    kind: str
-    start: SkipValidation[EdgePath]
-    end: SkipValidation[EdgePath]
-
-
-# @dataclass
-# class EdgePath:
-#     value: str
-#     match_by: str
-
-
-# @dataclass
-# class Edge:
-#     kind: str
-#     start: EdgePath
-#     end: EdgePath
 
 
 @graph_resource(
@@ -75,15 +46,9 @@ class RandomData(BaseResource):
             properties=properties,
         )
 
-    # @property
-    # def edges(self) -> Iterator[int]:
-    #     for index in range(100):
-    #         yield index
-
     @property
     def edges(self) -> Iterator[Edge]:
         for index in range(50):
-            end = EdgePath.model_construct(value="abc", match_by="id")
-            start = EdgePath.model_construct(value="abc", match_by="id")
-            yield 1
-            # yield Edge(start=start, end=end, kind="KnowsPerson")
+            end = EdgePath(value="abc", match_by="id")
+            start = EdgePath(value="abc", match_by="id")
+            yield Edge.model_construct(start=start, end=end, kind="KnowsPerson")
