9144a6f3889b97b94f2dd10c6054c8367e2016ee
Joey Dreijer <jdreijer@specterops.io>
2025-12-04 12:51:50 -0500

    Bootstrap rest for jamf/okta

--- opengraph_dlt/sources/jamf/source.py
+++ opengraph_dlt/sources/jamf/source.py
@@ -4,16 +4,18 @@ from dlt.sources.rest_api import rest_api_resources
 
 
 @dlt.source
-def okta_source(access_token=dlt.secrets.value, host=dlt.secrets.value):
+def jamf_source(
+    username=dlt.secrets.value, password=dlt.secrets.value, host=dlt.secrets.value
+):
     config: RESTAPIConfig = {
         "client": {
             "base_url": f"{host}/api/v1/",
             "auth": {
                 "type": "api_key",
                 "name": "Authorization",
-                "api_key": f"SSWS {access_token}",
+                # "api_key": f"SSWS {access_token}",
             },
         },
-        "resources": ["users", "groups", "iam/roles"],
+        "resources": ["users"],
     }
     yield from rest_api_resources(config)
